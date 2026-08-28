86fd3388e87efce2b94831f66459085a6f2c2237
rnmz <azure.rubykey@gmail.com>
2026-08-28 16:57:06 +0400

    wip

--- app/dto/file.go
+++ app/dto/file.go
@@ -3,13 +3,16 @@ package dto
 import "time"
 
 type FileInfoDTO struct {
-	Id         string     `json:"id"`
-	Crypt      bool       `json:"crypt"`
-	Name       string     `json:"name"`
-	Size       int64      `json:"size"` // MB
-	FolderId   string     `json:"folder_id"`
-	FolderName string     `json:"folder_name"`
-	UploadDate *time.Time `json:"upload_date"`
+	Id          string     `json:"id"`
+	Crypt       bool       `json:"crypt"`
+	Name        string     `json:"name"`
+	Size        int64      `json:"size"` // MB
+	FolderId    string     `json:"folder_id"`
+	FolderName  string     `json:"folder_name"`
+	AccessType  string     `json:"access"`
+	AccessLink  string     `json:"access_link"`
+	AccessUsers []string   `json:"access_users"`
+	UploadDate  *time.Time `json:"upload_date"`
 }
 
 type FileListDTO struct {
--- app/dto/folder.go
+++ app/dto/folder.go
@@ -1,4 +1,11 @@
 package dto
 
-type FolderCreateDTO struct {
+type FolderInfoDTO struct {
+	Id          string   `json:"id"`
+	Name        string   `json:"name"`
+	Size        int64    `json:"size"`
+	Crypt       string   `json:"crypt"`
+	AccessType  string   `json:"access"`
+	AccessLink  string   `json:"access_link"`
+	AccessUsers []string `json:"access_users"`
 }
