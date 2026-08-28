f948f347395ec8cb083d55c432f4f471c7e8f4a7
rnmz <azure.rubykey@gmail.com>
2026-08-28 16:04:33 +0400

    wip

--- app/router.go
+++ app/router.go
@@ -1,1 +1,24 @@
 package app
+
+import "github.com/gofiber/fiber/v3"
+
+func router(app *fiber.App) {
+	api := app.Group("/api", func() {})
+
+	api.Get("/file/list", func(c *fiber.Ctx) {})
+	api.Get("/file/get", func(c *fiber.Ctx) {})
+	api.Post("/file/upload", func(c *fiber.Ctx) {})
+	api.Delete("/file/delete", func(c *fiber.Ctx) {})
+
+	api.Post("/folder/create", func(c *fiber.Ctx) {})
+	api.Put("/file/update", func(c *fiber.Ctx) {})
+	api.Delete("/file/delete", func(c *fiber.Ctx) {})
+
+	auth := app.Group("/auth")
+	auth.Post("/login", func(c *fiber.Ctx) {})
+	auth.Post("/logout", func(c *fiber.Ctx) {})
+	auth.Post("/register", func(c *fiber.Ctx) {})
+
+	api.Post("/key/refresh", func(c *fiber.Ctx) {})
+	api.Post("/key/generate", func(c *fiber.Ctx) {})
+}
